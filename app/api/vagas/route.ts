464cf954978277d31c48f2c507b7c426de66fda1
mayconcoutinhodev <mayconcoutinhodev@gmail.com>
2026-02-24 10:35:44 -0300

    chore: reconfigurando prisma

--- app/api/vagas/route.ts
+++ app/api/vagas/route.ts
@@ -1,13 +1,12 @@
+export const runtime = "nodejs"; // aqui é nodejs, não node
+
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
-
 
 export async function GET() {
   try {
     const jobs = await prisma.job.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
+      orderBy: { createdAt: "desc" },
     });
 
     return NextResponse.json(jobs);
